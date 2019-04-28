fc7604c9a1785951dfbe7dc9d1fb431a4027cc5c
Douman <douman@gmx.se>
2019-04-28 15:03:10 +0300

    Forgot lib.rs update

--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,3 @@ pub fn is_jp<T: AsRef<str>>(text: T) -> bool {
     })
 }
 
-#[inline(always)]
-///Returns whether text contains only JP kana.
-pub fn is_jp_kana_only<T: AsRef<str>>(text: T) -> bool {
-    let text = text.as_ref();
-    text.chars().all(|elem_char| match elem_char { '\u{3000}'...'\u{303f}'| //punctuation
-                                                   '\u{3040}'...'\u{309f}'| //hiragana
-                                                   '\u{30a0}'...'\u{30ff}' //katakana
-                                                      => true,
-                                                   _  => false,
-    })
-}
